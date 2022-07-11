92641d7d6e7bfd58526394f4c0616f06a48007d5
Xavier Arpa <arpaxavier@gmail.com>
2022-07-11 10:32:27 +0200

    - Avances.

--- Unity_Notifications_Module.cs
+++ Unity_Notifications_Module.cs
@@ -26,6 +26,7 @@ namespace MMA.Unity_Notifications
         //[SerializeField] public ApplicationBase interface_Unity_Notifications;
         private readonly Dictionary<string, AndroidNotificationChannel> dic_notificationChannel = new Dictionary<string, AndroidNotificationChannel>();
         private readonly Dictionary<string, AndroidNotification> dic_notification = new Dictionary<string, AndroidNotification>();
+        private readonly Dictionary<string, int> dic_notificationSheduled = new Dictionary<string, int>();
 
         #endregion
         #region Reactions ( On___ )
@@ -67,7 +68,29 @@ namespace MMA.Unity_Notifications
             else dic_notification.Add(data.id, new AndroidNotification(data.title, data.text, data.delay, data.repeatInterval));
         }
 
-        private void SendNotification(string id) => AndroidNotificationCenter.SendNotification(dic_notification[id], id);
+        private void SendNotification(string id)
+        {
+            if (dic_notificationSheduled.ContainsKey(id))
+            {
+                if (AndroidNotificationCenter.CheckScheduledNotificationStatus(dic_notificationSheduled[id]) == NotificationStatus.Scheduled)
+                {
+                    // Replace the currently scheduled notification with a new notification.
+                    AndroidNotificationCenter.UpdateScheduledNotification(dic_notificationSheduled[id], dic_notification[id], id);
+                }
+                else if (AndroidNotificationCenter.CheckScheduledNotificationStatus(dic_notificationSheduled[id]) == NotificationStatus.Delivered)
+                {
+                    //Remove the notification from the status bar
+                    AndroidNotificationCenter.CancelNotification(dic_notificationSheduled[id]);
+                }
+                else if (AndroidNotificationCenter.CheckScheduledNotificationStatus(dic_notificationSheduled[id]) == NotificationStatus.Unknown)
+                {
+                    AndroidNotificationCenter.SendNotification(dic_notification[id], id);
+                }
+
+            } else {
+                dic_notificationSheduled.Add(id, AndroidNotificationCenter.SendNotification(dic_notification[id], id));
+            }
+        }
         #endregion
         #region Request ( Coroutines )
         // Contenedor de toda la Esperas de corutinas del Unity_Notifications
@@ -78,23 +101,3 @@ namespace MMA.Unity_Notifications
     }
 }
 
-
-/*
- 
- 
- if (AndroidNotificationCenter.CheckScheduledNotificationStatus(identifier) == NotificationStatus.Scheduled)
-        {
-            // Replace the currently scheduled notification with a new notification.
-            AndroidNotificationCenter.UpdateScheduledNotification(identifier, newNotification, channel);
-        }
-        else if (AndroidNotificationCenter.CheckScheduledNotificationStatus(identifier) == NotificationStatus.Delivered)
-        {
-            //Remove the notification from the status bar
-            AndroidNotificationCenter.CancelNotification(identifier);
-        }
-        else if (AndroidNotificationCenter.CheckScheduledNotificationStatus(identifier) == NotificationStatus.Unknown)
-        {
-            AndroidNotificationCenter.SendNotification(newNotification, "channel_id");
-        }
- 
- */
